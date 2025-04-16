beb1288443b8319cb3a6da82b7e887cddb5aada3
leyyenah <leyyenah@gmail.com>
2025-04-15 23:10:28 -0400

    updates

--- weather/urls.py
+++ weather/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'alert'
 
 urlpatterns = [
-    path('', views.weather_view, name='get_weather'),  
+    path('', views.weather_view, name='weather_view'),
     path('daily/<str:date>/', views.daily_detail_view, name='daily_detail_view'),
+    path('get-weather/', views.get_weather_by_coords, name='get_weather_by_coords'),
 ]
+
+
